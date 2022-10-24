f3c03aac2ad387b931ad5477bde156e44a3382de
Dcris <18218552814@163.com>
2022-10-24 19:52:04 +0800

    2022.10.24，完成两题，单调栈类型

---
+++ src/TowardsOffer2/day_13/code_039_直方图最大矩形面积/Solution.java
@@ -0,0 +1,46 @@
+package TowardsOffer2.day_13.code_039_直方图最大矩形面积;
+
+import java.util.*;
+
+/**
+ * completion time = 2022.10.24
+ */
+class Solution {
+    public int largestRectangleArea(int[] heights) {
+        if (heights.length == 1) {
+            return heights[0];
+        }
+        int n = heights.length;
+        Deque<Integer> stack = new ArrayDeque<>();
+        int[] left = new int[n];
+        int[] right = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            while (!stack.isEmpty() && heights[stack.peek()] >= heights[i]) {
+                stack.pop();
+            }
+            left[i] = stack.isEmpty() ? -1 : stack.peek();
+            stack.push(i);
+
+        }
+        stack.clear();
+        for (int i = n - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && heights[stack.peek()] >= heights[i]) {
+                stack.pop();
+            }
+            right[i] = stack.isEmpty() ? n : stack.peek();
+            stack.push(i);
+        }
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            ans = Math.max((right[i] - left[i] - 1) * heights[i], ans);
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2,1,5,6,2,3};
+        Solution solution = new Solution();
+        System.out.println(solution.largestRectangleArea(arr));
+    }
+}
---
+++ src/TowardsOffer2/day_13/code_040_矩阵中最大的矩形/Solution.java
@@ -0,0 +1,67 @@
+package TowardsOffer2.day_13.code_040_矩阵中最大的矩形;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * completion time = 2022.10.24
+ */
+class Solution {
+    public int maximalRectangle(String[] matrix) {
+        int m = matrix.length;
+        if (m == 0) {
+            return 0;
+        }
+        int n = matrix[0].length();
+        int[][] dp = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i].charAt(j) == '1') {
+                    dp[i][j] = j == 0 ? 1 : dp[i][j - 1] + 1;
+                }
+            }
+        }
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            // 寻找宽为dp[i][j]的长方形的最长高
+            int[] up = new int[m];
+            int[] down = new int[m];
+
+            Deque<Integer> stack = new ArrayDeque<>();
+            for (int j = 0; j < m; j++) {
+                if (dp[j][i] == 0) {
+                    stack.push(j);
+                    continue;
+                }
+                while (!stack.isEmpty() && dp[stack.peek()][i] >= dp[j][i]) {
+                    stack.pop();
+                }
+                up[j] = stack.isEmpty() ? -1 : stack.peek();
+                stack.push(j);
+            }
+            stack.clear();
+            for (int j = m - 1; j >= 0; j--) {
+                if (dp[j][i] == 0) {
+                    stack.push(j);
+                    continue;
+                }
+                while (!stack.isEmpty() && dp[stack.peek()][i] >= dp[j][i]) {
+                    stack.pop();
+                }
+                down[j] = stack.isEmpty() ? m : stack.peek();
+                stack.push(j);
+            }
+
+            for (int j = 0; j < m; j++) {
+                ans = Math.max(ans, dp[j][i] * (down[j] - up[j] - 1));
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        String[] arr = {"10100","10111","11111","10010"};
+        Solution solution = new Solution();
+        System.out.println(solution.maximalRectangle(arr));
+    }
+}
