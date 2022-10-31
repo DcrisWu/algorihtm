074e17400b04a4a3e064f1745455c07d4aff75d0
Dcris <18218552814@163.com>
2022-10-31 20:24:02 +0800

    2020.10.31，完成三道题，堆

---
+++ src/TowardsOffer2/day_20_堆/code_059_数据流的第K大数值/KthLargest.java
@@ -0,0 +1,44 @@
+package TowardsOffer2.day_20_堆.code_059_数据流的第K大数值;
+
+import java.util.PriorityQueue;
+
+/**
+ * completion time = 2022.10.31
+ */
+class KthLargest {
+
+    private PriorityQueue<Integer> queue = new PriorityQueue<>();
+    private int k = 0;
+    public KthLargest(int k, int[] nums) {
+        this.k = k;
+        for (int num : nums) {
+            if(queue.size() < k){
+                queue.add(num);
+            }else {
+                queue.add(num);
+                queue.poll();
+            }
+        }
+    }
+
+    public int add(int val) {
+        if(queue.size() < k){
+            queue.add(val);
+        }else {
+            queue.add(val);
+            queue.poll();
+        }
+        return queue.peek();
+    }
+
+    public static void main(String[] args) {
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+        int[] arr = {};
+        KthLargest kthLargest = new KthLargest(1, arr);
+        System.out.println(kthLargest.add(-3));
+        System.out.println(kthLargest.add(-2));
+        System.out.println(kthLargest.add(-4));
+        System.out.println(kthLargest.add(0));
+        System.out.println(kthLargest.add(4));
+    }
+}
---
+++ src/TowardsOffer2/day_20_堆/code_060_出现频率最高的k个数字/Solution.java
@@ -0,0 +1,43 @@
+package TowardsOffer2.day_20_堆.code_060_出现频率最高的k个数字;
+
+import java.util.*;
+
+/**
+ * completion time = 2022.10.31
+ */
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        PriorityQueue<Integer> treeSet = new PriorityQueue<>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return map.getOrDefault(o1, 0) - map.getOrDefault(o2, 0);
+
+            }
+        });
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        for (Integer integer : map.keySet()) {
+            if (treeSet.size() < k) {
+                treeSet.add(integer);
+            } else {
+                treeSet.add(integer);
+                treeSet.poll();
+            }
+        }
+        int[] ans = new int[k];
+        for (int i = k - 1; i >= 0; i--) {
+            ans[i] = treeSet.poll();
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] arr = {4, 1, -1, 2, -1, 2, 3};
+        for (int i : solution.topKFrequent(arr, 2)) {
+            System.out.println(i);
+        }
+    }
+}
