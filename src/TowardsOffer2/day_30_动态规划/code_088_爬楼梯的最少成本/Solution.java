3967fe1ad14cc262dd55f4a3a2a568727992d5d4
Dcris <18218552814@163.com>
2022-11-09 10:14:45 +0800

    2020.11.9，完成三道题，动态

---
+++ src/TowardsOffer2/day_30_动态规划/code_088_爬楼梯的最少成本/Solution.java
@@ -0,0 +1,24 @@
+package TowardsOffer2.day_30_动态规划.code_088_爬楼梯的最少成本;
+
+/**
+ * completion time = 2022.11.9
+ */
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        /*int[] dp = new int[cost.length];
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+        for (int i = 2; i < dp.length; i++) {
+            dp[i] = Math.min(dp[i - 1], dp[i - 2]) + cost[i];
+        }
+        return Math.min(dp[dp.length - 1], dp[dp.length - 2]);*/
+        int pre1 = cost[0];
+        int pre2 = cost[1];
+        for (int i = 2; i < cost.length; i++) {
+            int now = Math.min(pre1, pre2) + cost[i];
+            pre1 = pre2;
+            pre2 = now;
+        }
+        return Math.min(pre1, pre2);
+    }
+}
---
+++ src/TowardsOffer2/day_31_动态规划/code_091_粉刷房子/Solution.java
@@ -0,0 +1,9 @@
+package TowardsOffer2.day_31_动态规划.code_091_粉刷房子;
+
+class Solution {
+    public int minCost(int[][] costs) {
+        // 0-red, 1-blue, 2-green
+        // dp存的是每件房子选择的颜色
+        return 0;
+    }
+}
