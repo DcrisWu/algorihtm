8750fe9a80bd603e26e0db4e316d75caa369a6a9
Dcris <18218552814@163.com>
2022-11-13 16:01:31 +0800

    2020.11.13，完成5道题，动态规划

---
+++ src/TowardsOffer2/day_33_动态规划/code_098_路径的数目/Solution.java
@@ -0,0 +1,22 @@
+package TowardsOffer2.day_33_动态规划.code_098_路径的数目;
+
+/**
+ * completion time = 2022.11.13
+ */
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            dp[i][0] = 1;
+        }
+        for (int i = 1; i < n; i++) {
+            dp[0][i] = 1;
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+}
