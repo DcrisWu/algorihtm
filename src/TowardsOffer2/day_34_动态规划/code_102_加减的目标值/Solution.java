7f04d77473c03ddd7c3e68745fbfe544102a5d19
Dcris <18218552814@163.com>
2022-11-14 11:31:22 +0800

    2020.11.14，完成3道题，动态规划

---
+++ src/TowardsOffer2/day_34_动态规划/code_102_加减的目标值/Solution.java
@@ -0,0 +1,60 @@
+package TowardsOffer2.day_34_动态规划.code_102_加减的目标值;
+
+import java.sql.Struct;
+import java.util.Arrays;
+
+class Solution {
+    public int findTargetSumWays(int[] nums, int target) {
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        int m = nums.length;
+        int n = sum * 2 + 1;
+        if (target + sum >= n || target + sum < 0) {
+            return 0;
+        }
+        int[] pre = new int[n];
+        int[] now = new int[n];
+        pre[sum - nums[0]] += 1;
+        pre[sum + nums[0]] += 1;
+        for (int i = 1; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                now[j] = 0;
+                if (j - nums[i] >= 0) {
+                    now[j] += pre[j - nums[i]];
+                }
+                if (j + nums[i] < n) {
+                    now[j] += pre[j + nums[i]];
+                }
+            }
+            int[] temp = now;
+            now = pre;
+            pre = temp;
+        }
+        return pre[target + sum];
+        /*int[][] dp = new int[m][n];
+        dp[0][sum - nums[0]] += 1;
+        dp[0][sum + nums[0]] += 1;
+        for (int i = 1; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (j - nums[i] >= 0) {
+                    dp[i][j] += dp[i - 1][j - nums[i]];
+                }
+                if (j + nums[i] < n) {
+                    dp[i][j] += dp[i - 1][j + nums[i]];
+                }
+            }
+        }
+        if(target + sum >= n || target + sum < 0){
+            return 0;
+        }
+        return dp[nums.length - 1][target + sum];*/
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1};
+        Solution solution = new Solution();
+        System.out.println(solution.findTargetSumWays(arr, 2));
+    }
+}
---
+++ src/TowardsOffer2/day_35_动态规划/code_103_最少的硬币数目/Solution.java
@@ -0,0 +1,33 @@
+package TowardsOffer2.day_35_动态规划.code_103_最少的硬币数目;
+
+import java.util.Arrays;
+
+/**
+ * completion time = 2022.11.14
+ */
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        if(amount == 0){
+            return 0;
+        }
+        Arrays.sort(coins);
+        int[] dp = new int[amount + 1];
+        for (int i = 1; i < dp.length; i++) {
+            for (int j = 0; j < coins.length && i - coins[j] >= 0; j++) {
+                if (i - coins[j] == 0) {
+                    dp[i] = 1;
+                } else {
+                    if (i - coins[j] != 0 && dp[i - coins[j]] != 0) {
+                        if (dp[i] == 0) {
+                            dp[i] = dp[i - coins[j]] + 1;
+                        }else {
+                            dp[i] = Math.min(dp[i],dp[i - coins[j]] + 1);
+                        }
+                    }
+                }
+            }
+        }
+        return dp[amount] == 0 ? -1 : dp[amount];
+    }
+
+}
