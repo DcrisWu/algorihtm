13429a69d682fccad63402a94b9d3b6bf896594e
Dcris <18218552814@163.com>
2022-11-28 12:11:21 +0800

    2020.11.27-28，完成8道题

---
+++ src/TowardsOffer/day_7_搜索与回溯算法_简单/code_28_对称的二叉树/Solution.java
@@ -0,0 +1,33 @@
+package TowardsOffer.day_7_搜索与回溯算法_简单.code_28_对称的二叉树;
+
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return dfs(root.left, root.right);
+    }
+
+    boolean dfs(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        }
+        if (left == null || right == null) {
+            return false;
+        }
+        if (left.val != right.val) {
+            return false;
+        }
+        return dfs(left.left, right.right) && dfs(left.right, right.left);
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
---
+++ src/competition/day_2022_11_27/code_6246_追加字符以获得子序列/Solution.java
@@ -0,0 +1,14 @@
+package competition.day_2022_11_27.code_6246_追加字符以获得子序列;
+
+class Solution {
+    public int appendCharacters(String s, String t) {
+        int i = 0, j = 0;
+        while (i < s.length() && j < t.length()) {
+            if (s.charAt(i) == t.charAt(j)) {
+                j++;
+            }
+            i++;
+        }
+        return j == t.length() ? 0 : t.length() - j;
+    }
+}
