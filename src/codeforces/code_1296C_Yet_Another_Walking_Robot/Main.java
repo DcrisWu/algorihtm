4511fd546c4f47a9a8ac8dc2704d88f06986a214
Dcris <18218552814@163.com>
2023-04-12 18:18:28 +0800

    2023.4.12 2道题

---
+++ src/codeforces/code_1296C_Yet_Another_Walking_Robot/Main.java
@@ -0,0 +1,51 @@
+package codeforces.code_1296C_Yet_Another_Walking_Robot;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+/**
+ * completion time = 2023.4.12
+ */
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
+        for (int i = 0; i < t; i++) {
+            int n = scanner.nextInt();
+            String str = scanner.next();
+            // 第一层的key表示 L & R,L用 -1，R用 +1
+            // 第二层的key表示 U & P,U用 +1, D用 -1
+            // 最后的值表示下标，初始化下标为0，表示在str.charAt(i)前的位置，不包含str.charAt(i)
+            Map<Integer, Map<Integer, Integer>> map = new HashMap<>();
+            map.put(0, new HashMap<>());
+            map.get(0).put(0, 0);
+            int x = 0, y = 0;
+            int l = -1, r = -1;
+            for (int j = 0; j < n; j++) {
+                if (str.charAt(j) == 'L') {
+                    x -= 1;
+                } else if (str.charAt(j) == 'R') {
+                    x += 1;
+                } else if (str.charAt(j) == 'U') {
+                    y += 1;
+                } else {
+                    y -= 1;
+                }
+                if (map.containsKey(x)) {
+                    if (map.get(x).containsKey(y)) {
+                        if (l == -1 || j + 1 - map.get(x).get(y) < r - l) {
+                            l = map.get(x).get(y) + 1;
+                            r = j + 1;
+                        }
+                    }
+                } else {
+                    map.put(x, new HashMap<>());
+                }
+                map.get(x).put(y, j + 1);
+            }
+            System.out.println(l == -1 ? -1 : l + " " + r);
+
+        }
+    }
+}
---
+++ src/codeforces/code_1473D_Program/Main.java
@@ -0,0 +1,53 @@
+package codeforces.code_1473D_Program;
+
+import java.util.*;
+
+/**
+ * completion time = 2023.4.12
+ * comment:这题还是不是很懂
+ */
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
+        for (int i = 0; i < t; i++) {
+            // 字符串长度为n
+            int n = scanner.nextInt();
+            // 操作m次
+            int m = scanner.nextInt();
+            String str = scanner.next();
+            int[] preSum = new int[n + 1];  // 前缀和
+            // prefix[i] 表示 nums[i]之前的和，不包括nums[i]
+            int[] prefixMin = new int[n + 1];   // 前缀最小值
+            int[] prefixMax = new int[n + 1];   // 前缀最大值
+            for (int j = 0; j < n; j++) {
+                int num = str.charAt(j) == '+' ? 1 : -1;
+                preSum[j + 1] = preSum[j] + num;
+                prefixMin[j + 1] = Math.min(prefixMin[j], preSum[j] + num);
+                prefixMax[j + 1] = Math.max(prefixMax[j], preSum[j] + num);
+            }
+            // suffix[i]表示preSum[i]到最后一个元素之间的最小值和最大值，包括nums[i-1]
+            int[] suffixMin = new int[n + 1];
+            int[] suffixMax = new int[n + 1];
+            int postSum = preSum[n];
+            suffixMin[n] = postSum;
+            suffixMax[n] = postSum;
+            for (int j = n - 1; j >= 0; j--) {
+                int num = str.charAt(j) == '+' ? -1 : 1;
+                postSum += num;
+                suffixMin[j] = Math.min(suffixMin[j + 1], postSum);
+                suffixMax[j] = Math.max(suffixMax[j + 1], postSum);
+            }
+            for (int j = 0; j < m; j++) {
+                int l = scanner.nextInt();
+                int r = scanner.nextInt();
+                // 因为中间删除了一段，所以只需要将后半段下降一段距离就可以了，这段距离 = 中间段尾部高度 - 中间段头部高度
+                // 然后取两段的最小值和最大值，再相减
+                int cha = preSum[r] - preSum[l - 1];
+                int min = Math.min(prefixMin[l - 1], suffixMin[r] - cha);
+                int max = Math.max(prefixMax[l - 1], suffixMax[r] - cha);
+                System.out.println(max - min + 1);
+            }
+        }
+    }
+}
