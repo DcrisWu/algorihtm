ef2124a635bdcc8a10afed985edf996f8d2264e7
DcrisWu <dcris@foxmail.com>
2023-07-27 20:01:55 +0800

    复习LRU

--- src/Random/code_146_LRU缓存/LRUCache.java
+++ src/Random/code_146_LRU缓存/LRUCache.java
@@ -10,88 +10,89 @@ import java.util.Map;
  * 淘汰的时候，只需要将链表最后一个节点删除，然后同时淘汰HashMap中的键值对
  * <p>
  * completion time = 2023.4.17
+ * <p>
+ * LRU需要用链表来快速删除节点，同时添加到头节点
+ * 第二次做题 completion time = 2023.7.27
  */
 class LRUCache {
 
-    private static class Node {
+    private class Node {
         int key;
-        int value;
+        int val;
         Node pre;
         Node next;
-
-        public Node() {
-        }
-
-        public Node(int key, int value, Node pre, Node next) {
-            this.key = key;
-            this.value = value;
-            this.pre = pre;
-            this.next = next;
-        }
-
-        public Node(int key, int value) {
-            this.key = key;
-            this.value = value;
-        }
     }
 
-    void delete(Node node) {
-        Node pre = node.pre;
-        Node next = node.next;
-        pre.next = next;
-        next.pre = pre;
+    private void addPeek(Node node) {
+        Node temp = head.next;
+        head.next = node;
+        temp.pre = node;
+        node.pre = head;
+        node.next = temp;
     }
 
-    void addTail(Node node) {
-        Node pre = tail.pre;
-        pre.next = node;
-        node.pre = pre;
-        node.next = tail;
-        tail.pre = node;
+    // 淘汰最后一个，并返回最后一个的key
+    private int deleteTail() {
+        Node needDelete = end.pre;
+        needDelete.pre.next = end;
+        end.pre = needDelete.pre;
+        return needDelete.key;
     }
 
-    private int capacity;
+    private void deleteNode(Node node) {
+        node.pre.next = node.next;
+        node.next.pre = node.pre;
+    }
+
+    // head和end不作为实际使用
     private Node head;
-    private Node tail;
-    private Map<Integer, Node> map;
+    private Node end;
+    private int capacity;
+    private Map<Integer, Node> lru;
 
     public LRUCache(int capacity) {
         this.capacity = capacity;
-        Node head = new Node(-1, -1);
-        Node tail = new Node(-1, -1, head, null);
-        head.next = tail;
-        this.head = head;
-        this.tail = tail;
-        this.map = new HashMap<>();
+        this.lru = new HashMap<>();
+        head = new Node();
+        end = new Node();
+        head.next = end;
+        end.pre = head;
     }
 
     public int get(int key) {
-        if (!map.containsKey(key)) {
+        if (!lru.containsKey(key)) {
             return -1;
         }
-        Node node = map.get(key);
-        delete(node);
-        addTail(node);
-        return node.value;
+        Node node = lru.get(key);
+        deleteNode(node);
+        addPeek(node);
+        return node.val;
     }
 
     public void put(int key, int value) {
-        // 如果键已经存在，就变更数值
-        if (map.containsKey(key)) {
-            Node node = map.get(key);
-            node.value = value;
-            delete(node);
-            addTail(node);
+        if (lru.containsKey(key)) {
+            Node node = lru.get(key);
+            node.val = value;
+            deleteNode(node);
+            addPeek(node);
         } else {
-            // 如果容量已经满了，就需要淘汰一个
-            if (map.size() >= capacity) {
-                Node last = head.next;
-                delete(last);
-                map.remove(last.key);
+            // 直接插入就可以
+            if (lru.size() < capacity) {
+                Node node = new Node();
+                node.key = key;
+                node.val = value;
+                lru.put(key, node);
+                addPeek(node);
+            } else {
+                // 需要淘汰最后一个再插入
+                int lastKey = deleteTail();
+                lru.remove(lastKey);
+                Node node = new Node();
+                node.key = key;
+                node.val = value;
+                lru.put(key, node);
+                addPeek(node);
             }
-            Node node = new Node(key, value);
-            map.put(key, node);
-            addTail(node);
         }
     }
 }
